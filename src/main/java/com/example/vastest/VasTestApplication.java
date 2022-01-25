1396f8f851a09ffbd8fb34b4422cf278ce81310f
LuisGalanR <lgruiz.97@gmail.com>
2022-01-25 20:49:13 +0100

    Initial Commit
    Spring Initializer generation

---
+++ src/main/java/com/example/vastest/VasTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vastest/VasTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vastest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
