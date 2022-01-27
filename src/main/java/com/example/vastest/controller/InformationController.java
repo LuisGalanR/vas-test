6acac851c280174cbe2c80360447d1e8d0169494
LuisGalanR <lgruiz.97@gmail.com>
2022-01-27 16:12:27 +0100

    Autowired change for heroku deploy

--- src/main/java/com/example/vastest/controller/InformationController.java
+++ src/main/java/com/example/vastest/controller/InformationController.java
@@ -3,6 +3,7 @@ package com.example.vastest.controller;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import com.example.vastest.utils.Validators;
 @RestController()
 public class InformationController {
 
+	@Autowired
 	private InformationService informationService;
 
 	public InformationController(InformationService informationService) {
